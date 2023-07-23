7adc3708181ecfc4f7e6bd1fc570f0ab0b3ddd21
Rajvardhan shinde <110803326+rajvardhanshinde@users.noreply.github.com>
2023-07-23 16:40:31 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,10 @@
+function services(){
+
+   document.querySelector(".lis").style.display="block";
+   document.querySelector(".lis").style.cursor="pointer" ;
+
+}
+function lservices(){
+
+   document.querySelector(".lis").style.display="none";
+}
